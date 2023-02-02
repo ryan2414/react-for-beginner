bc76dd46e1d0462d854531cb533e69bbfddcb268
ryan2414 <jackpar@nate.com>
2023-02-02 22:28:44 +0900

    Movie App

--- src/App.js
+++ src/App.js
@@ -1,52 +1,42 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [dl, setDl] = useState(0);
-  const onChange = (event) => {
-    setDl(event.target.value);
-  };
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (dl === "") {
-      return;
-    }
-  };
-  useEffect(() => {
-    fetch("	https://api.coinpaprika.com/v1/tickers")
-      .then((resonse) => resonse.json())
-      .then((json) => {
-        setCoins(json);
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const getMovies = async () =>{
+        const response = await fetch(
+            `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+        );
+        const json = await response.json();
+        setMovies(json.data.movies);
         setLoading(false);
-      });
-  }, []);
-  return (
+
+    }
+    useEffect(() =>{
+        getMovies()
+    }, []);
+    return(
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={dl}
-          type="number"
-          placeholder="Input USD"
-        ></input>
-      </form>
-      <hr />
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            <option key={coin.id}>
-              {coin.name} ({coin.symbol}): {dl / coin.quotes.USD.price}{" "}
-              {coin.symbol}
-            </option>
-          ))}
-        </select>
-      )}
+        {loading ? (
+            <h1>Loading...</h1>
+            ) :(
+            <div>
+                {movies.map((movie) => (
+                    <div key={movie.id}>
+                        <img src={movie.medium_cover_image} />
+                        <h2>{movie.title}</h2>
+                        <p>{movie.summary}</p>
+                        <ul>
+                            {movie.genres.map((g) =>(
+                                <li key={g}>{g}</li>
+                            ))}
+                        </ul>
+                    </div>
+                ))}
+            </div>
+        )}
     </div>
-  );
+    );
 }
 
 export default App;
